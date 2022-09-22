db42c9c5f97446a0b9752a9e3301b5f7300e4e65
EanPhillips8 <eanphillips8@gmail.com>
2022-09-22 17:36:00 -0400

    s

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int birthMonth = (11);
+        System.out.println("What is your birth month [1-12]:");
+
+        if (birthMonth>= 1 && birthMonth <= 12)
+        {
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        else
+        {
+            System.out.println("Error, please enter a valid birth month: ");
+        }
+
     }
 }
